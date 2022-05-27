f059463085c3c849ec4a6cc29454e5492c353165
vvallade <vincent.vallade@lip6.fr>
2022-05-27 19:19:08 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if [[ "${COMP_S3_PROBLEM_PATH}" == *".xz" ]];
-then
-  aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf.xz
-  unxz supervised-scripts/test.cnf.xz
-else
-  aws s3 cp s3://${S3_BKT}/${COMP_S3_PROBLEM_PATH} supervised-scripts/test.cnf
-fi
-
-time /painless/painless-mcomsps -c=31 -solver=k -shr-sleep=750000 -shr-strat=1 supervised-scripts/test.cnf
+/painless/painless-mcomsps -c=30 -solver=k -shr-sleep=750000 -shr-strat=1 $2
